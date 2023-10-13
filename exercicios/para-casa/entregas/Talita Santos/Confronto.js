f68464ea3f92050e7cb052a1f5b3a54f9723bc07
talu <talita.santos96@hotmail.com>
2023-10-13 12:13:10 -0300

    [async] Refatora variáveis de cálculo do poder das personagens

--- exercicios/para-casa/entregas/Talita Santos/Confronto.js
+++ exercicios/para-casa/entregas/Talita Santos/Confronto.js
@@ -1,14 +1,14 @@
 export class Confronto {
     lutar(superHeroi, vilao) {
-        let heroiVencedor = superHeroi.poderTotal();
-        let vilaoVencedor = vilao.poderTotal();
+        let poderDoHeroi = superHeroi.poderTotal();
+        let poderDoVilao = vilao.poderTotal();
 
-        if (heroiVencedor > vilaoVencedor) {
+        if (poderDoHeroi > poderDoVilao) {
             console.log(1);
         } else {
             console.log(2);
         }
-        if (heroiVencedor === vilaoVencedor) {
+        if (poderDoHeroi === poderDoVilao) {
             console.log(0);
         }
     }
